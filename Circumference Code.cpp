a976bff19045c3319713192d00ceeec769471a29
qahirali0705 <qahirali0705@gmail.com>
2026-07-05 10:35:13 +0500

    Add files via upload

---
+++ Circumference Code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	float pi=3.14;
+	int Radius=5;
+	
+	float Circumference=2*pi*Radius;
+	
+	
+	cout << "The circumference of Radius Is "<<Circumference<<endl;
+	
+	return 0;
+}
